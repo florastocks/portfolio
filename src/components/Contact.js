6fc8ce39580e83296fcb3742dbc3311093ca4224
florastocks <stocksflora@gmail.com>
2022-10-23 12:54:26 +0100

    little styling added to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,30 +18,32 @@ const Contact = () => {
   // );
   return (
       <>
-        <h1 className='page-heading'>Get in Touch</h1> 
-        <div className='cards-wrapper'>
-          <div className="card">
-            <div className="card-body">
-              <h3 className="card-title">LinkedIn</h3>
-              <p classname='card-para'>Please do react out to me on LinkedIn. You can also visit my profile to find a greater list of previous experience, and my skills</p>
-              <button className="btn-contact"><a href='https://www.linkedin.com/in/flora-stocks/'><img className='round-img' src={LinkedIn} alt='LinkedIn Logo'/></a></button>
+        <section className='page-wrapper'>
+          <h1 className='page-heading'>Get in Touch</h1> 
+          <div className='cards-wrapper'>
+            <div id='LinkedIn-card'className="card">
+              <div className="card-body">
+                <h3 className="card-title">LinkedIn</h3>
+                <p classname='card-para'>React me on LinkedIn. Here you will find a list of previous experience, and my skills.</p>
+                <button className="btn-contact"><a href='https://www.linkedin.com/in/flora-stocks/'><img className='round-img' src={LinkedIn} alt='LinkedIn Logo'/></a></button>
+              </div>
+            </div>
+            <div id='email-card' className="card">
+              <div className="card-body">
+                <h3 className="card-title">Email Me</h3>
+                <p classname='card-para'>Feel free to Email me anytime by pressing the button below!</p>
+                <button className="btn-contact"><a href="mailto:stocksflora@gmail.com"><img className='round-img' src={EmailImg} alt='Email Logo'/></a></button>
+              </div>
+            </div>
+            <div id='GitHub-card'className="card">
+              <div className="card-body">
+                <h3 className="card-title">GitHub</h3>
+                <p classname='card-para'>Ulternatively, if you just wish to view my work then please visit my GitHub below.</p>
+                <button className="btn-contact"><a href='https://github.com/florastocks'><img className='round-img' src={GitHubImg} alt='GitHub Logo'/></a></button>
+              </div>
             </div>
           </div>
-          <div className="card">
-            <div className="card-body">
-              <h3 className="card-title">Email Me</h3>
-              <p classname='card-para'>Feel free to Email me anytime by pressing the button below!</p>
-              <button className="btn-contact"><a href="mailto:stocksflora@gmail.com"><img className='round-img' src={EmailImg} alt='Email Logo'/></a></button>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <h3 className="card-title">GitHub</h3>
-              <p classname='card-para'>Ulternatively, if you just wish to view my work then please visit my GitHub below.</p>
-              <button className="btn-contact"><a href='https://github.com/florastocks'><img className='round-img' src={GitHubImg} alt='GitHub Logo'/></a></button>
-            </div>
-          </div>
-        </div>
+        </section>
       </>
   )
 }
