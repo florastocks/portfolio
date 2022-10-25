2c42705040e9ef78f7b3a292cff4a89c6a0ab0f0
florastocks <stocksflora@gmail.com>
2022-10-25 14:00:40 +0100

    flora stocks navigates to about me

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,21 @@
 import Me from '../images/Me.png'
 import { Col } from 'react-bootstrap'
-
+import { useNavigate } from 'react-router-dom' 
 
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  const handleClick = (event) => {
+    navigate('/about')
+  }
+
   return (
     <div className="homepage-welcome">
       <Col className='md-6'>
         <h4>Hello, my name is</h4>
-        <h1>Flora Stocks</h1>
+        <h1 onClick={handleClick}>Flora Stocks</h1>
         <h4>Junior Software Engineer / Web Developer</h4>
       </Col>
       <Col classNamw='homePhoto'>
